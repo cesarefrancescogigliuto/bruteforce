c59ec1fe34b4f8a48cc276baf6d225225650c8bc
cesarefrancescogigliuto <cesaregigliuto08@gmail.com>
2022-11-09 12:16:15 +0100

    First commit

---
+++ algoritmo.py
@@ -0,0 +1,44 @@
+# Password generator
+# Author: Cesare Francesco Gigliuto 
+# Author: Ciro Cozzolino
+# Version: 1.0
+
+# imports
+import itertools
+import sys
+import os
+import itertools
+from datetime import datetime as dt
+import random
+from random import randint 
+import string
+
+dictionary = ['a','s','d','f','g','h','j', 'k']
+random.seed(dt.now())
+
+print('\n\n*** GENERATORE DI PASSWORD CASUALI *** \n')
+def creapassword():
+    a = int(input("inserire la lunghezza della password da generare: "))
+
+    if a == 0 :
+        print("input non valido") 
+        return ""
+
+    b = ""
+
+    for i in range(0,a):
+        r = randint(0,len(dictionary)-1)
+        b = b + dictionary[r]
+
+    return b
+
+def get_random_string(length, dicti):
+    # choose from all lowercase letter
+    return ''.join(random.choice(dicti) for i in range(length))
+
+def creaCampione(password, elements):
+    out = []
+    for i in range(1,elements):
+        out.append(get_random_string(len(password), dictionary))
+
+    return out
